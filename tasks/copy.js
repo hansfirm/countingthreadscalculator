76b2d784d34c4c1bebfbedc79e5f6306c5b084e6
HB <baggetthayden@yahoo.com>
2021-03-03 20:14:21 -0600

    first commit

---
+++ tasks/copy.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+
+module.exports = () => {
+  return gulp.src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/popper.js/dist/popper.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js',
+    'src/js/tooly.js'
+  ])
+    .pipe(gulp.dest('./docs/js'))
+};
