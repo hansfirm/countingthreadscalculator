d46bd8335604d57253bbaac78f3cbc88cbbdfccc
HB <baggetthayden@yahoo.com>
2021-03-25 19:10:58 -0500

    myrrh yes

--- docs/js/quizStyle.js
+++ docs/js/quizStyle.js
@@ -1,5 +1,5 @@
 
-  
+
   var current_fs, next_fs, previous_fs; //fieldsets
   var opacity;
   var current = 1;
@@ -9,7 +9,6 @@
   
   $(".next").click(function(){
   
-  console.log('Hello World');
   current_fs = $(this).parent();
   next_fs = $(this).parent().next();
   
@@ -73,4 +72,30 @@
   $(".submit").click(function(){
   return false;
   })
-  
+
+
+
+
+  ///translating tooly!
+  var horas = new Date().getHours();
+if (horas >= 0 && horas < 12)
+    {var relativeTime = "morning";}
+else if (horas >= 12 && horas < 18)
+    {var relativeTime = "afternoon";}
+else 
+    {var relativeTime = "evening";}
+
+
+
+var i = 0;
+var txt = `Good ${relativeTime} – I'm here to help determine your annual fashion footprint! Answer the following questions to see how your clothing affects the environment and economy.`; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+window.onload = function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+ 
